8a26e1d7d689155d024c8c5ff30f6f62b4def21b
lananotrey <lananotrey@outlook.com>
2025-04-29 11:40:14 -0700

    Command SwiftCompile failed with a nonzero exit code
    /Users/workcar/Documents/gen project/FitTrack/FitTrack/Generated/FitTrack/Generated/FitTrack/Generated/Models/Goal.swift
    /Users/workcar/Documents/gen project/FitTrack/FitTrack/Generated/FitTrack/Generated/FitTrack/Generated/Models/Goal.swift:3:8 Type 'Goal' does not conform to protocol 'Decodable'
    
    /Users/workcar/Documents/gen project/FitTrack/FitTrack/Generated/FitTrack/Generated/FitTrack/Generated/Models/Goal.swift:3:8 Type 'Goal' does not conform to protocol 'Encodable'
    
    /Users/workcar/Documents/gen project/FitTrack/FitTrack/Generated/FitTrack/Generated/FitTrack/Generated/Models/Goal.swift:3:8 Invalid redeclaration of 'Goal'
    
    /Users/workcar/Documents/gen project/FitTrack/FitTrack/Generated/FitTrack/Generated/FitTrack/Generated/Models/Goal.swift:6:15 'GoalType' is ambiguous for type lookup in this context
    
    /Users/workcar/Documents/gen project/FitTrack/FitTrack/Generated/FitTrack/Generated/FitTrack/Generated/Models/Goal.swift:12:50 'GoalType' is ambiguous for type lookup in this context
    
    /Users/workcar/Documents/gen project/FitTrack/FitTrack/Generated/FitTrack/Generated/FitTrack/Generated/Models/Goal.swift:23:6 Invalid redeclaration of 'GoalType'

--- FitTrack/Generated/FitTrack/Generated/Models/WorkoutModel.swift
+++ FitTrack/Generated/FitTrack/Generated/Models/WorkoutModel.swift
@@ -36,32 +36,6 @@ enum WorkoutType: String, CaseIterable, Codable {
     }
 }
 
-struct Goal: Identifiable, Codable {
-    let id: UUID
-    let title: String
-    let type: GoalType
-    let target: Int
-    let deadline: Date
-    var manualProgress: Double?
-    var isCompleted: Bool
-    
-    init(id: UUID = UUID(), title: String, type: GoalType, target: Int, deadline: Date, manualProgress: Double? = nil, isCompleted: Bool = false) {
-        self.id = id
-        self.title = title
-        self.type = type
-        self.target = target
-        self.deadline = deadline
-        self.manualProgress = manualProgress
-        self.isCompleted = isCompleted
-    }
-}
-
-enum GoalType: String, CaseIterable, Codable {
-    case workouts = "Workouts Completed"
-    case minutes = "Minutes Exercised"
-    case calories = "Calories Burned"
-}
-
 enum TimeFrame: String, CaseIterable {
     case week = "Week"
     case month = "Month"
